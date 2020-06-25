e073a8817c3b6c94367f2baa657f37c71d59f53d
kar655 <karol123zag@gmail.com>
2020-06-25 22:39:47 +0200

    Add functions to Expression class

--- src/logicalOperations/Expression.java
+++ src/logicalOperations/Expression.java
@@ -9,4 +9,40 @@ public abstract class Expression {
     protected int priority() {
         return Priorities.EXPRESSION.getPriority();
     }
+
+    public Expression neg() {
+        return new Negation(this);
+    }
+
+    protected Expression andReversed(Expression expression) {
+        return new OperationAnd(expression, this);
+    }
+
+    public Expression and(Expression expression) {
+        return expression.andReversed(this);
+    }
+
+    protected Expression orReversed(Expression expression) {
+        return new OperationOr(expression, this);
+    }
+
+    public Expression or(Expression expression) {
+        return expression.orReversed(expression);
+    }
+
+    protected Expression xorReversed(Expression expression) {
+        return new OperationXor(expression, this);
+    }
+
+    public Expression xor(Expression expression) {
+        return expression.xor(this);
+    }
+
+    protected Expression xorTrue(Expression expression) {
+        return new OperationXor(expression, this);
+    }
+
+    protected Expression xorFalse(Expression expression) {
+        return new OperationXor(expression, this);
+    }
 }
--- src/logicalOperations/Negation.java
+++ src/logicalOperations/Negation.java
@@ -1,4 +1,23 @@
 package logicalOperations;
 
-public class Negation {
+public class Negation extends Expression {
+
+    private Expression expression;
+
+    public Negation(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public boolean evaluate(boolean... values) {
+        return !expression.evaluate(values);
+    }
+
+    @Override
+    public String toString() {
+        if (expression.priority() == Priorities.EXPRESSION.getPriority())
+            return "~" + expression;
+        else
+            return "~(" + expression + ")";
+    }
 }
